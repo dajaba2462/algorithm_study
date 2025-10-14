17dd4da982c2ed63a587e197d604e811226aeb9b
Doyoung Kim <dajaba2462@naver.com>
2025-10-14 17:15:34 +0900

    10810 and 10871(fix)

--- baekjoon_10871.py
+++ baekjoon_10871.py
@@ -8,13 +8,12 @@
 
 출력
 X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다."""
-string = ""
+
 N, X = map(int, input().split())
 A = list(map(int,input().split()))
 for i in range(len(A)):
     if X > A[i]:
-        string += str(A[i]) + " "
-print(string)
+        print(A[i], end =' ')
 
     
 
